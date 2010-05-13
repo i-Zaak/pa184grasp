2390a3b7a505efbc05094333414267f588ff3dda
harfor@centrum.cz <harfor@centrum.cz@b1ea4ec3-ce9d-e6f2-1840-285d30c7787e>
2010-05-13 00:27:20 +0000

    Parser and initial version of interface - second try

--- GAP-grasp/src/gap/GapParser.java
+++ GAP-grasp/src/gap/GapParser.java
@@ -5,10 +5,69 @@
 
 package gap;
 
-/**
- *
- * @author Salla
- */
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
 public class GapParser {
 
+    File file;
+    
+    public GapParser(String _file){
+        file = new File("./data/"+_file);
+    }
+    
+    public GapProblem parseProblem(int pos) throws FileNotFoundException, IOException{
+        BufferedReader in = new BufferedReader(new FileReader(file));
+        
+        String line; 
+        line = in.readLine();
+        
+        if (pos > Integer.parseInt(line))
+            return null; // we want something that is not there
+        in.readLine(); // first is always empty 
+        while (pos > 1){            
+            if (in.readLine().isEmpty()) {
+                pos--;
+                if (pos == 1) // time to read problem
+                    break;
+                while(in.readLine().isEmpty()) { // skip empty lines                    
+                }                
+            }    
+        }        
+        while((line = in.readLine()).isEmpty()){ // skip empty lines    
+        }            
+        // our problem is here
+        String tmp[] = line.split(" ");
+        int workersCount = Integer.valueOf(tmp[0]);
+        int jobCount = Integer.valueOf(tmp[1]);
+        GapProblem myProblem = new GapProblem(workersCount,jobCount);
+        
+        for (int i = 0; i < workersCount; i++) { // reading costs
+            line = in.readLine();
+            tmp = line.split(" ");
+            for (int j=0; j < tmp.length; j++){
+                myProblem.setWorkerJobCost(i, j, Integer.parseInt(tmp[j]));
+            }
+        }
+        
+        for (int i = 0; i < workersCount; i++) { // reading time
+            line = in.readLine();
+            tmp = line.split(" ");
+            for (int j=0; j < tmp.length; j++){
+                myProblem.setWorkerJobTime(i, j, Integer.parseInt(tmp[j]));
+            }
+        }
+        
+        line = in.readLine();
+        tmp = line.split(" ");    
+        for (int i=0; i < tmp.length; i++){
+            myProblem.setWorkerLimitTime(i, Integer.parseInt(tmp[i]));
+        }
+        
+        return myProblem;
+    }
 }
--- GAP-grasp/src/gap/Main.java
+++ GAP-grasp/src/gap/Main.java
@@ -5,17 +5,22 @@
 
 package gap;
 
-/**
- *
- * @author Salla
- */
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
 public class Main {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        GapParser parser= new GapParser("gap4.txt"); //files in ./data/ dir
+        System.out.println("Reading information...");
+        GapProblem myProblem = parser.parseProblem(5); // 5th example in file
+        System.out.println("Done");
+        myProblem.assign(1, 1);
+        
+        System.out.println(myProblem.toString());        
     }
-
 }
