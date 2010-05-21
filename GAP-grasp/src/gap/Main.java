d2cba21b45c9f093a55b2084fd66f6b202c781f5
fousek <fousek@b1ea4ec3-ce9d-e6f2-1840-285d30c7787e>
2010-05-21 16:46:56 +0000

    Greedy solution generation without backtracking.

--- GAP-grasp/src/gap/Main.java
+++ GAP-grasp/src/gap/Main.java
@@ -16,17 +16,18 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        GapParser parser= new GapParser("gap4.txt"); //files in ./data/ dir
+        GapParser parser= new GapParser("gap1.txt"); //files in ./data/ dir
         System.out.println("Reading information...");
-        GapProblem myProblem = parser.parseProblem(5); // 5th example in file
+        GapProblem myProblem = parser.parseProblem(3); // 5th example in file
         System.out.println("Done");
         long runtime = new Date().getTime();
         boolean solved = myProblem.generateRandomSolution();
+        //boolean solved = myProblem.generateGreedySolution();
         runtime = new Date().getTime() - runtime;
         if (solved){
             System.out.println(myProblem.toString()); 
             System.out.println("Solution found in " + runtime + " ms with " + myProblem.getBacktracksCount() + " backtracks"); 
-        }else{
+        }else{System.out.println(myProblem.toString()); 
              System.out.println("No solution:(");  
         }
     }
