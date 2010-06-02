f8ad943fefb456635a6445440a9970ffdf1500e1
pavel@ics.muni.cz <pavel@ics.muni.cz@b1ea4ec3-ce9d-e6f2-1840-285d30c7787e>
2010-06-02 11:45:08 +0000

    Backtracking and fallbacks for peckish solution generator

--- trunk/GAP-grasp/src/gap/GapProblem.java
+++ trunk/GAP-grasp/src/gap/GapProblem.java
@@ -8,101 +8,120 @@ import java.util.Vector;
 import java.util.Comparator;
 
 public class GapProblem {
-    
+
     private int workersCount;
-    private int jobsCount;  
+    private int jobsCount;
     private GapSolution solution;
-    private ArrayList<LinkedList<Integer>> jobDomains = new ArrayList<LinkedList<Integer>>(); 
+    private ArrayList<LinkedList<Integer>> jobDomains = new ArrayList<LinkedList<Integer>>();
     int backtracksCount;
-    
-    public GapProblem(int _workersCount, int _jobsCount, GapSettings _settings){
+
+    public GapProblem(int _workersCount, int _jobsCount, GapSettings _settings) {
         workersCount = _workersCount;
         jobsCount = _jobsCount;
         solution = new GapSolution(jobsCount, workersCount, _settings);
-        for (int i=0; i < jobsCount; i++) {                       
+        for (int i = 0; i < jobsCount; i++) {
             jobDomains.add(new LinkedList<Integer>());
-            for (int j=0; j < workersCount; j++) {
-                // domains
-                jobDomains.get(i).add(new Integer(j));
-            }
         }
+        fillJobDomains();
         backtracksCount = 0;
     }
 
     // Comparator for Job class, compares by minTime in descending order
     public static Comparator<Job> JOB_MINTIME_ORDER_DESC = new Comparator<Job>() {
+
         public int compare(Job j1, Job j2) {
             int minTime1 = j1.getMinTime();
             int minTime2 = j2.getMinTime();
-            if (minTime1 < minTime2) return -1;
-            else if (minTime2 < minTime1) return 1;
-            else return 0;
+            if (minTime1 < minTime2) {
+                return -1;
+            } else if (minTime2 < minTime1) {
+                return 1;
+            } else {
+                return 0;
+            }
         }
     };
 
-    
-    @Override // lame, but it will do for now
-    public String toString(){
-        return solution.toString();        
+    private void fillJobDomains() {
+        for (int i = 0; i < jobsCount; i++) {
+            jobDomains.clear();
+            for (int j = 0; j < workersCount; j++) {
+                jobDomains.get(i).add(new Integer(j));
+            }
+        }
     }
- 
-    public boolean generateRandomSolution(){
-        arcConsistency(-1); 
+
+    @Override // lame, but it will do for now
+
+    public String toString() {
+        return solution.toString();
+    }
+
+    public boolean generateRandomSolution() {
+        arcConsistency(-1);
         Random generator = new Random();
-        
-        for (int i = 0; i < jobsCount; i++){
-            if(!jobDomains.get(i).isEmpty()){
+
+        for (int i = 0; i < jobsCount; i++) {
+            if (!jobDomains.get(i).isEmpty()) {
                 int pos = generator.nextInt(jobDomains.get(i).size());
                 int worker = jobDomains.get(i).get(pos).intValue();
-                solution.assign(i,worker,true);
+                solution.assign(i, worker, true);
                 jobDomains.get(i).remove(pos);
                 arcConsistency(-1); // arc consistency on all not assigned variables
-            } else{
+
+            } else {
                 i = i - 1; // unassign previous
-                if (i < 0){
+
+                if (i < 0) {
                     return false; // no solution
+
                 }
                 backtracksCount++;
                 solution.unassign(i);
-                arcConsistency(i);   
+                arcConsistency(i);
                 i = i - 1; // just step back in for cycle to get to the unassign variable       
+
             }
-       }
-       return true; 
-    }  
-    //the main idea is to clear infeasible values from not assigned variables except of the ommited one = node we are backtracking to
-    private void arcConsistency(int ommit){
-        GapSettings set = solution.getSettings();
-        for (int i=0; i < jobsCount; i++) {
-            if (solution.isAssigned(i) || i == ommit)
-                continue;
-            jobDomains.get(i).clear();
-            for (int j=0; j < workersCount; j++) {
-               if (set.getLimitTime(j) >= (solution.getWorkerTime(j) + set.getCost(j,i)))
-                    jobDomains.get(i).add(new Integer(j));  // this value can be used!         
-            }
-        }        
+        }
+        return true;
     }
-    
-    public int getBacktracksCount(){
+    //the main idea is to clear infeasible values from not assigned variables except of the ommited one = node we are backtracking to
+
+    private void arcConsistency(int ommit) {
+        GapSettings set = solution.getSettings();
+        for (int i = 0; i < jobsCount; i++) {
+            if (solution.isAssigned(i) || i == ommit) {
+                continue;
+            }
+            jobDomains.get(i).clear();
+            for (int j = 0; j < workersCount; j++) {
+                if (set.getLimitTime(j) >= (solution.getWorkerTime(j) + set.getCost(j, i))) {
+                    jobDomains.get(i).add(new Integer(j));  // this value can be used!         
+
+                }
+            }
+        }
+    }
+
+    public int getBacktracksCount() {
         return backtracksCount;
     }
-       
-    public boolean generateGreedySolution(){
+
+    public boolean generateGreedySolution() {
         GapSettings set = solution.getSettings();
         Vector<Job> sortedJobs = new Vector<Job>(jobsCount);
         int minTime, maxTime, bestWorker, time;
-        for(int job=0; job < jobsCount; job++){
+        for (int job = 0; job < jobsCount; job++) {
             minTime = -1;
             maxTime = -1;
             bestWorker = -1;
-            
-            for(int worker=0; worker < workersCount; worker++){
-                time = set.getTime(worker,job);
-                if(maxTime < time){
-                    maxTime = time;                    
+
+            for (int worker = 0; worker < workersCount; worker++) {
+                time = set.getTime(worker, job);
+                if (maxTime < time) {
+                    maxTime = time;
                 }
-                if(time < minTime || minTime == -1){
+                if (time < minTime || minTime == -1) {
                     minTime = time;
                     bestWorker = worker;
                 }
@@ -111,45 +130,57 @@ public class GapProblem {
         }
         Collections.sort(sortedJobs);
         System.out.println(sortedJobs);
-        for(int i=0; i < jobsCount; i++){
+        for (int i = 0; i < jobsCount; i++) {
             Job job = sortedJobs.get(i);
-            if(!solution.assign(job.getId(),job.getBestWorkerId())){
+            if (!solution.assign(job.getId(), job.getBestWorkerId())) {
                 boolean assigned = false;
-                for(int j=0; j < workersCount; j++){
-                    if(solution.assign(job.getId(),j)){
+                for (int j = 0; j < workersCount; j++) {
+                    if (solution.assign(job.getId(), j)) {
                         assigned = true;
                         break;
                     }
                 }
-                if(!assigned){
+                if (!assigned) {
                     System.out.println("Failed to assign job " + job.getId());
                     System.out.println("FIXME: implement backtracking in Greedy solution generation!");
-                    return false; 
+                    return false;
                 }
             }
         }
-        
+
         return true;
     }
-    
+
     public boolean generatePeckishSolution() {
         return generatePeckishSolution(0.2);
     }
-    
+
     /* The function generates feasible peckish solution of the problem. Given
      * ratio of jobs (those considered the hardest) is assigned to the best
      * possible worker (if the assignemnt is feasible), the others are assigned
      * randomly. */
     public boolean generatePeckishSolution(double ratio) {
         GapSettings set = solution.getSettings();
+        if (ratio < 0.0) {
+            ratio = 0.0;
+        }
+        if (ratio > 1.0) {
+            ratio = 0.2;
+        }
         int greedyJobs = (int) (ratio * jobsCount);
+        // Selected ratio too low, fall back to random
+        if (greedyJobs <= 0) {
+            System.out.println("Peckish generator: ratio too low, fallback to radnom generation.");
+            return generateRandomSolution();
+        }
+
         Vector<Job> sortedJobs = new Vector<Job>(jobsCount);
         int minTime, bestWorker, time;
         minTime = bestWorker = -1;
-        
+
         // Determine the shortest time needed for completion of each job.
         for (int job = 0; job < jobsCount; job++) {
-            
+
             for (int worker = 0; worker < workersCount; worker++) {
                 time = set.getTime(worker, job);
                 if (time < minTime || minTime == -1) {
@@ -165,115 +196,171 @@ public class GapProblem {
         Collections.sort(sortedJobs, JOB_MINTIME_ORDER_DESC);
         // Output for a visual check.
         System.out.println(sortedJobs);
-        
+
+        //Create new vector of jobs that shall be assigned randomly
+        Vector<Job> randomJobs = new Vector<Job>(sortedJobs);
+        // Assign jobs selected for greedy assignment
         for (int i = 0; i < greedyJobs; i++) {
             Job job = sortedJobs.get(i);
-            // Assign job to the worker. Don't check if it was actually assigned,
-            // we won't consider that at all.
-            solution.assign(job.getId(), job.getBestWorkerId());
+            // If possigle, assign job to the best worker and remove the job from
+            // randomJobs vector
+            if (solution.assign(job.getId(), job.getBestWorkerId())) {
+                randomJobs.remove(job);
+            }
         }
-        
-        //TODO: random assignment of the rest, fallback to random if everything
-        //fails
+
+        // how many times have we run?
+        int level = 0;
+        Random generator = new Random();
+        while (level < 3 && greedyJobs > 5) {
+            // Reset job domains
+            fillJobDomains();
+            // Remove infeasible values on the basis of previous assignments
+            arcConsistency(-1);
+            // Iterate through randomJobs and attempt to assign them randomly
+            for (int i = 0; i < randomJobs.size(); i++) {
+                int jobId = randomJobs.get(i).getId();
+                if (!jobDomains.get(i).isEmpty()) {
+                    int workerPos = generator.nextInt(jobDomains.get(jobId).size());
+                    int workderId = jobDomains.get(jobId).get(workerPos).intValue();
+                    solution.assign(jobId, workderId);
+                    jobDomains.get(jobId).remove(workerPos);
+                    arcConsistency(-1);
+                } else { //we have no option to assign the job, hence backtracking
+
+                    i--;
+                    if (i < 0) {
+                        break; //no solution found
+
+                    }
+                    solution.unassign(jobId);
+                    arcConsistency(jobId);
+                    backtracksCount++;
+                    i--;
+                }
+            }
+            // If all variables are assigned, we are done.
+            if (solution.allAssigned()) {
+                return true;
+            }
+            // Otherwise, start another iteration
+            level++;
+            int newGreedyJobs = greedyJobs / 2;
+            // Move some jobs from greedy assignment to random assigment
+            for (int i = newGreedyJobs; i < greedyJobs; i++) {
+                // Unassign the job
+                solution.unassign(sortedJobs.get(i).getId());
+                // Add the job to randomJobs vector
+                randomJobs.add(sortedJobs.get(i));
+            }
+            // Sort the randomJobs vector again
+            Collections.sort(randomJobs, JOB_MINTIME_ORDER_DESC);
+            greedyJobs = newGreedyJobs;
+        }
+        // Failed to assign few times, falling back to random
+        if (!solution.allAssigned()) {
+            System.out.println("Peckish generator: FAILED, fallback to radnom generation.");
+            return generateRandomSolution();
+        }
+        //This should never happen:
+        System.out.println("Peckish generator: something went very wrong");
         return true;
     }
 
-    public void clear(){
-      solution.clear();  
-      backtracksCount = 0;   
+    public void clear() {
+        solution.clear();
+        backtracksCount = 0;
     }
-    
+
     //shift in solution
-    public boolean perturbate(){
+    public boolean perturbate() {
         int first_worker = solution.getWorker(0);
-        for (int i = 0; i < jobsCount -1; i++){
+        for (int i = 0; i < jobsCount - 1; i++) {
             solution.unassign(i);
-            solution.assign(i, solution.getWorker(i+1), true);            
+            solution.assign(i, solution.getWorker(i + 1), true);
         }
-        solution.unassign(jobsCount -1);
-        solution.assign(jobsCount -1, first_worker, true);
-        return solution.isFeasible();           
+        solution.unassign(jobsCount - 1);
+        solution.assign(jobsCount - 1, first_worker, true);
+        return solution.isFeasible();
     }
-    
-        //change workers posession
-    public boolean perturbate2(){
-        
-        for (int i = 0; i < jobsCount -1; i++){
+
+    //change workers posession
+    public boolean perturbate2() {
+
+        for (int i = 0; i < jobsCount - 1; i++) {
             int prev_worker = solution.unassign(i);
-            solution.assign(i, (prev_worker+1) % workersCount, true);            
+            solution.assign(i, (prev_worker + 1) % workersCount, true);
         }
-        return solution.isFeasible();           
+        return solution.isFeasible();
     }
-    
-    
-    
-    public GapSolution getSolution(){
+
+    public GapSolution getSolution() {
         return solution;
     }
-    
-    public void setSolution(GapSolution _solution){
+
+    public void setSolution(GapSolution _solution) {
         solution = _solution;
     }
-    
-    public int getCostLowerBound(){
+
+    public int getCostLowerBound() {
         GapSettings set = solution.getSettings();
         int minimal_global_cost = 0;
         for (int i = 0; i < jobsCount; i++) {
             int min = Integer.MAX_VALUE;
-            for (int j = 0; j < workersCount; j++){
-                if (min > set.getCost(j, i)){
+            for (int j = 0; j < workersCount; j++) {
+                if (min > set.getCost(j, i)) {
                     min = set.getCost(j, i);
                 }
             }
             minimal_global_cost += min;
-        }      
+        }
         return minimal_global_cost;
     }
-    
+
     //change one worker to get neighbour
-    public GapSolution getBestNeighbour(){
+    public GapSolution getBestNeighbour() {
         double bestCost = solution.getPenalty();
-        GapSolution bestSolution = new GapSolution(solution,solution.getSettings());
-        for (int i = 0; i < jobsCount; i++){
-            GapSolution neighSolution = new GapSolution(solution,solution.getSettings());
+        GapSolution bestSolution = new GapSolution(solution, solution.getSettings());
+        for (int i = 0; i < jobsCount; i++) {
+            GapSolution neighSolution = new GapSolution(solution, solution.getSettings());
             int old_worker = neighSolution.getWorker(i);
-            for (int j = 0; j < workersCount; j++){
-                if (j != old_worker){
+            for (int j = 0; j < workersCount; j++) {
+                if (j != old_worker) {
                     neighSolution.unassign(i);
                     neighSolution.assign(i, j, true);
                     double cost = neighSolution.getPenalty();
                     if (cost < bestCost) {
-                        bestSolution = new GapSolution(neighSolution,solution.getSettings());
+                        bestSolution = new GapSolution(neighSolution, solution.getSettings());
                         bestCost = cost;
                     }
                 }
             }
-            
-        }  
+
+        }
         return bestSolution;
     }
-    
+
     // My take on greedy algrithm with backtracing
-        public boolean generateGreediestSolution(){
+    public boolean generateGreediestSolution() {
         GapSettings set = solution.getSettings();
-        arcConsistency(-1); 
+        arcConsistency(-1);
         double[] min_cost = new double[jobsCount];
         int[] jobs = new int[jobsCount];
-        
-        for (int i = 0; i < jobsCount; i++){
+
+        for (int i = 0; i < jobsCount; i++) {
             jobs[i] = i;
             int min = Integer.MAX_VALUE;
-            for (int j = 0; j < workersCount; j++){
-                if (min > set.getCost(j, i)){
+            for (int j = 0; j < workersCount; j++) {
+                if (min > set.getCost(j, i)) {
                     min = set.getCost(j, i);
-                    min_cost[i] = min;                  
+                    min_cost[i] = min;
                 }
-            }    
+            }
         }
 
-        for (int i = 0; i < jobsCount - 1; i++){
-            for (int j = i + 1; j < jobsCount; j++){
-                 if (min_cost[i] > min_cost[j]){
+        for (int i = 0; i < jobsCount - 1; i++) {
+            for (int j = i + 1; j < jobsCount; j++) {
+                if (min_cost[i] > min_cost[j]) {
                     double tmp = min_cost[i];
                     min_cost[i] = min_cost[j];
                     min_cost[j] = tmp;
@@ -283,34 +370,38 @@ public class GapProblem {
                 }
             }
         }
-        
-           for (int i = 0; i < jobsCount; i++){
+
+        for (int i = 0; i < jobsCount; i++) {
             int job = jobs[i];
-            if(!jobDomains.get(job).isEmpty()){
-               int min = Integer.MAX_VALUE; 
-               int best_pos = -1;
-               for (int j = 0; j < jobDomains.get(job).size(); j++){
-                   int worker = jobDomains.get(job).get(j).intValue();
-                   if (min > set.getCost(worker,job)){
-                        min = set.getCost(worker,job);
-                        best_pos = j; 
+            if (!jobDomains.get(job).isEmpty()) {
+                int min = Integer.MAX_VALUE;
+                int best_pos = -1;
+                for (int j = 0; j < jobDomains.get(job).size(); j++) {
+                    int worker = jobDomains.get(job).get(j).intValue();
+                    if (min > set.getCost(worker, job)) {
+                        min = set.getCost(worker, job);
+                        best_pos = j;
                     }
-               } 
-               int worker = jobDomains.get(job).get(best_pos).intValue();
-                solution.assign(job,worker,true);
+                }
+                int worker = jobDomains.get(job).get(best_pos).intValue();
+                solution.assign(job, worker, true);
                 jobDomains.get(job).remove(best_pos);
                 arcConsistency(-1); // arc consistency on all not assigned variables
-            } else{
+
+            } else {
                 i = i - 1; // unassign previous
-                if (i < 0){
+
+                if (i < 0) {
                     return false; // no solution
+
                 }
                 backtracksCount++;
                 solution.unassign(jobs[i]);
-                arcConsistency(jobs[i]);   
+                arcConsistency(jobs[i]);
                 i = i - 1; // just step back in for cycle to get to the unassign variable       
+
             }
         }
-        return true; 
-    } 
+        return true;
+    }
 }
